3493af35a45b10356f19060c6aac9b16c36aad94
G <116308628+gashaw11@users.noreply.github.com>
2024-11-23 22:11:53 +0000

    i adjusted the code

--- indoor/playback.py
+++ indoor/playback.py
@@ -1,1 +1,16 @@
 #add three dots to the end of each word from an input
+playback = input()
+
+
+def main():
+    words = playback.split()
+    print('...'.join(words))
+ 
+if __name__ == "__main__":
+    main()
+
+
+
+
+# playbackoutput='...'.join([i + '...' for i in playbackinput.split()])
+# print(playbackoutput)
