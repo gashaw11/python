3f2fafe331b4e2c26ab51ce01845b95bfd40ad5f
G <116308628+gashaw11@users.noreply.github.com>
2024-11-27 13:52:42 -0500

    Create deep.py

---
+++ deep/deep.py
@@ -0,0 +1,8 @@
+
+#conditions
+
+deep = input("he answer to the Great Question of Life, the Universe and Everything?").strip().lower()
+if deep!="42" and deep!= "forty two" and deep !="forty-two":
+    print ("no")
+else:
+    print ("yes")
