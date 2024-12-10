9cd9d5c750d5823761905f1d6a61c93e9a756050
G <116308628+gashaw11@users.noreply.github.com>
2024-12-10 10:49:44 -0500

    Update figlet.py

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,18 +1,22 @@
-submit50 cs50/problems/2022/python/figlet
+#submit50 cs50/problems/2022/python/figlet
 
 from pyfiglet import Figlet
 import random
 import sys
 figlet = Figlet()
-text= input("Input: ")
+
 if len(sys.argv)==1:
-    print(figlet.renderText(text))
-elif len(sys.argv) == 2:
-    font = sys.argv[1]
-    if font in figlet.getFonts():
-        figlet.setFont(font=font)
-        print(fglet.renderText(text))
-else:
     random_font = random.choice(figlet.getFonts())
     figlet.setFont(font=random_font)
+    text= input("Input: ")
     print(figlet.renderText(text))
+
+
+elif len(sys.argv) == 3 and sys.argv[1] in["-f","--font"]:
+    font = sys.argv[2]
+    if font in figlet.getFonts():
+        figlet.setFont(font=font)
+        text = input("Enter text to render: ")
+        print(fglet.renderText(text))
+else:
+    sys.exit(1)
