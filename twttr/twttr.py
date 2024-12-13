121358a90ad9626170a4d9c40f9d37fd29304b33
G <116308628+gashaw11@users.noreply.github.com>
2024-12-13 11:33:03 -0500

    Update twttr.py

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,8 +1,17 @@
 #submit50 cs50/problems/2022/python/twttr
-#using join and list comprehnssion
+
+def main():
+
+    user_input = input("Input: ")
+    output = shorten(user_input)
+    print(f"Output: {output}")
 
 
 
-Input = input("Input: ")
-Output = ''.join(c for c in Input if c not in "aeiouAEIOU")
-print(Output)
+def shorten(user_input):
+    output = ''.join(c for c in user_input if c not in "aeiouAEIOU")
+    return output
+
+
+if ___name__=="__main__":
+    main()
