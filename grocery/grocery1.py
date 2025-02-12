bad1ac746ce0c8e49975c953b70bf422ecc8b825
Gashaw <gmirtu@gmail.com>
2025-02-12 16:38:55 -0500

    please save this

---
+++ grocery/grocery1.py
@@ -0,0 +1,12 @@
+grocery = {}
+while True:
+
+    try:
+        item = input().strip().upper()
+        grocery[item]=grocery.get(item,0)+1
+        
+    except EOFError:
+        break
+
+for item in sorted(grocery):
+    print(grocery[item],item
