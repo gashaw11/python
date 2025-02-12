230d1062bb46458d046b777ebcda746d95079c97
Gashaw <gmirtu@gmail.com>
2025-02-12 05:51:23 -0500

    csv

--- merchandise/csv_writer.py
+++ merchandise/csv_writer.py
@@ -1,19 +1,4 @@
-import csv
-
-def write_csv(filename):
-    with open(filename, 'w', newline = '') as file:
-        writer = csv.writer(file)
-
-        header = input("Enter header row")
-        if header:
-            writer.writerow(header.split(','))
-        while True:
-            row = input("Enter data row")
-            if row.lower() == 'done':
-                break
-            writer.writerow(row.split(','))
-
-if __name__=="__main__":
-    filename = input("enter a file name")
-    write_csv(filename)
-    print("sucees")
+Item,Quantity
+pen,56
+tshirt,68
+chair,45
