7db6765c460e4fa2fba4adb89df2a97466fd154d
Gashaw <gmirtu@gmail.com>
2025-02-12 09:08:45 -0500

    updatecsv

--- merchandise/update_csv.py
+++ merchandise/update_csv.py
@@ -5,9 +5,9 @@ df = pd.read_csv("merchandise.csv")
 
 # Dictionary of updates: {Category: New Amount}
 updates = {
-    "Groceries": -60.00,
-    "Transport": -35.00,
-    "Shopping": -100.00
+    "pen": -60.00,
+    "tshirt": -35.00,
+    "laptop": -100.00
 }
 
 # Apply bulk updates where the Category matches
