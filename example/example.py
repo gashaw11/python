32da48a1990bbedba29b64db4b2ecb9c28d9866f
G <116308628+gashaw11@users.noreply.github.com>
2025-02-18 17:07:57 +0000

    save

--- example/example.py
+++ example/example.py
@@ -1,3 +1,32 @@
-import random
-n = random.random()
-print(n)
+# import random
+
+# #m = int(input("enter the number of times "))
+
+# while True:
+#     probability = input("do you want another number? ")
+#     if probability.upper() == "YES":
+        
+#             n = random.random()
+#             #m -= 1
+
+#             print(n)
+#     else:
+#         break
+    
+
+
+
+# class Person:
+#    def __init__(self, name, age):
+#       self.name = name
+#       self.age = age
+
+#    def __repr__(self):
+#       return f"{self.name},{self.age}"
+
+# p = Person("ali",42)
+# print(p)
+
+
+
+
